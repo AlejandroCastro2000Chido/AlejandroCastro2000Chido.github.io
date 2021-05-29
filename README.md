e788abf276d86370c404437047f611a875e57626
AlejandroCastro2000Chido <alejandro.castro2000.cacr@gmail.com>
2021-05-29 18:00:52 -0500

    agregado

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# AlejandroCastro2000Chido.github.io
-Intento
+# rickchat
+Ejemplo de un chat y acceso a Firebase (Firestore con Google)
---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDV-VhWEG2N7-K4HWHPdS4L41xQipJRb5Y",
+    authDomain: "chatc3cv50.firebaseapp.com",
+    projectId: "chatc3cv50",
+    storageBucket: "chatc3cv50.appspot.com",
+    messagingSenderId: "664645241209",
+    appId: "1:664645241209:web:70978a18b3cdb22ec6c7a8",
+    measurementId: "G-FHGR3KXX7J"
+});
